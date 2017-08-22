7a404535f75bd2191ac95dc3d965be99d809ae86
Manu22012 <30653679+Manu22012@users.noreply.github.com>
2017-08-22 23:27:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res){
+    res.send('Article one requested');
+});
+app.get('/article-2',function(req,res){
+    res.send('Article one requested');
+});
+app.get('/article-3',function(req,res){
+    res.send('Article one requested');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
