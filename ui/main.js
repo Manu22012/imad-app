4180c2d1bfd18b1361775f465fd6c1ad72e2edf8
Manu22012 <30653679+Manu22012@users.noreply.github.com>
2017-08-27 10:12:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById("Main");
 element.innerHTML="NEW ELE";
-var img=document.getElementById("img");
+var img=document.getElementById("madi");
 img.onclick=function(){
     img.style.marginLeft="100px";
 };
